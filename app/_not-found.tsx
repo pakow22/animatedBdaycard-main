d453f604850109b187741d44100c65edf1b353ad
pakow22 <42393235+pakow22@users.noreply.github.com>
2025-11-12 17:52:16 +0400

    Fix 404 prerender + client components

---
+++ app/_not-found.tsx
@@ -0,0 +1,16 @@
+export default function NotFound() {
+  return (
+    <div style={{
+      width: "100%",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "24px",
+      fontWeight: "bold",
+      background: "white"
+    }}>
+      Page not found
+    </div>
+  );
+}
